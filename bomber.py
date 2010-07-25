8c98886209350c6b2689d768177cc2a6d3c75dba
Adam Piper <adam@ahri.net>
2010-07-25 11:20:03 +0000

    incremental changes, players can now move

--- bomber.py
+++ bomber.py
@@ -6,81 +6,224 @@ Bomberman Clone
 
 from collections import deque
 from udeque import udeque
+import os
 
-class GameState(object):
+class Arena(object):
+    def __init__(self):
+        self.arena = []
+
+    def traverse(self):
+        """
+        an iterable for traversing the game arena
+        """
+        for y, row in enumerate(self.arena):
+            for x, tile in enumerate(row):
+                yield x, y, tile
+
+    def coord_sanity(self, x, y):
+        if x < 0 or y < 0:
+            raise IndexError("Negative indices don't make sense")
+
+    def tile_get(self, x, y):
+        """
+        access the values of tiles in the game arena
+        """
+        self.coord_sanity(x, y)
+        return self.arena[y][x]
+
+    def tile_set(self, coords, value):
+        """
+        set the values of tiles in the game arena
+        """
+        self.coord_sanity(*coords)
+        x, y = coords
+        self.arena[y][x] = value
+
+    def tile_has(self, coords, item):
+        """
+        detect whether a tile has a certain game item in it
+        everything has a 0 in it, unfortunately this evaluates to false
+        """
+        if item == 0:
+            self.coord_sanity(*coords)
+            return True
+
+        return self.tile_get(*coords) & item
+
+    def tile_add(self, coords, item):
+        """
+        add an item to a tile
+        """
+        return self.tile_set(coords, self.tile_get(*coords) | item)
+
+    def tile_remove(self, coords, item):
+        """
+        remove an item from a tile
+        """
+        self.tile_set(coords, self.tile_get(*coords) ^ item)
+
+class GameState(Arena):
     """
     Represents the game state
     """
+
+    SPACE = 0
+    DESTRUCTABLE = 1
+    BLOCK = 2
+    SPAWN = 4
+    P1 = 8
+    P2 = 16
+    P3 = 32
+    P4 = 64
+    P5 = 128
+    P6 = 256
+    P7 = 512
+    P8 = 1024
+
     def __init__(self):
         """
         simple init of variables
         """
-        self._arena = """BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-BS ................................. SB
-B B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B B
-BS ................................. SB
-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"""
-        self._players = udeque()
+        self.players = udeque()
+        self.players_ingame = []
 
-    def load_arena(self, arena_file):
+        self.lookup = {
+            ' ': self.SPACE,
+            '.': self.DESTRUCTABLE,
+            'B': self.BLOCK,
+            'S': self.SPAWN,
+            '1': self.P1,
+            '2': self.P2,
+            '3': self.P3,
+            '4': self.P4,
+            '5': self.P5,
+            '6': self.P6,
+            '7': self.P7,
+            '8': self.P8,
+        }
+
+        self.player_list = [
+            self.P1,
+            self.P2,
+            self.P3,
+            self.P4,
+            self.P5,
+            self.P6,
+            self.P7,
+            self.P8,
+        ]
+
+        self.rlookup = {}
+        for k, v in self.lookup.iteritems():
+            self.rlookup[v] = k
+
+        self.arena_load(["arenas", "default.bmm"])
+
+    def arena_load(self, filename_list):
         """
         load an arena from a file
         """
-        arena = open(arena_file, 'r')
-        self._arena = arena.read()
-        arena.close()
+        fp = open(os.sep.join(filename_list), 'r')
 
-    def get_arena(self):
-        return self._arena
+        line_length = 0
+        lines = []
+        for line_number, line in enumerate(fp):
+            if len(line) > line_length:
+                line_length = len(line)
 
-    arena = property(get_arena, load_arena)
+            lines.append(line)
 
-    def add_player(self, player):
+        fp.close()
+
+        self.arena = [[self.SPACE for x in range(line_length - 1)]
+                                  for y in range(line_number + 1)]
+
+        for y, line in enumerate(lines):
+            for x, char in enumerate(line):
+                if char == "\n":
+                    break
+
+                self.tile_set((x, y), self.lookup[char])
+
+    def __str__(self):
+        """
+        produce a string representation of the arena
+        in the same format as the map files
+        """
+        chars = []
+        old_y = 0
+        for x, y, tile in self.traverse():
+            if y != old_y:
+                chars.append('\n')
+                old_y = y
+
+            chars.append(self.rlookup[tile])
+
+        chars.append('\n')
+
+        return ''.join(chars)
+
+    def player_add(self, player):
         """
         add a player to the game state
         """
-        self._players.appendleft(player)
+        self.players.appendleft(player)
 
     def get_players(self):
         """
         get currently loaded players
         """
-        return self._players
+        return self.players
 
     def spawn(self):
         """
         spawn the players into the arena
-        TODO: move this to Arena class
         """
-        p_no = 1
-        arena = ''
-        for tile in self.arena:
-            if tile == 'S' and len(self._players):
-                arena += p_no.__str__()
-                player = self._players.pop()
-                player.number = p_no
+        p_no = 0
+        for x, y, tile in self.traverse():
+            if tile == self.SPAWN and len(self.players):
+                self.tile_set((x, y), self.player_list[p_no])
+                player = self.players.pop()
+                player.number = p_no + 1
+                player.item = self.player_list[p_no]
+                player.coords = (x, y)
+                self.players_ingame.append(player)
                 p_no += 1
-            else:
-                arena += tile
-
-        self._arena = arena
 
     def tick(self):
-        pass
+        """
+        step to the next game state
+        """
+        for player in self.players_ingame:
+            px, py = player.coords
+            nx, ny = -1, -1
+
+            if player.action_current() == None:
+                continue
+            elif player.action_current() == Player.UP:
+                nx, ny = px, py-1
+            elif player.action_current() == Player.DOWN:
+                nx, ny = px, py+1
+            elif player.action_current() == Player.LEFT:
+                nx, ny = px-1, py
+            elif player.action_current() == Player.RIGHT:
+                nx, ny = px+1, py
+
+            if (nx, ny) != (-1, -1):
+                self.player_move(player, (nx, ny))
+
+
+    def player_move(self, player, new_coords):
+        """
+        move a player
+        """
+        if self.tile_has(new_coords, self.BLOCK) or self.tile_has(new_coords, self.DESTRUCTABLE):
+            return
+
+        self.tile_remove(player.coords, player.item)
+        self.tile_add(new_coords, player.item)
+        player.coords = new_coords
+
 
 class Player(object):
     """
@@ -96,15 +239,21 @@ class Player(object):
 
     def __init__(self):
         self.number = 0
-        self._actions = deque()
+        self.actions = deque()
+        self.current = None
+        self.coords = ()
+        self.item =  -1
 
     def action_next(self):
-        return self._actions.pop()
+        return self.actions.pop()
 
     def action_add(self, action):
-        self._actions.appendleft(action)
+        self.actions.appendleft(action)
 
-    act = property(action_next, action_add)
+    def action_current(self):
+        try:
+            self.current = self.action_next()
+        except IndexError:
+            pass
 
-class Arena(object):
-    pass
+        return self.current
--- test_bomber.py
+++ test_bomber.py
@@ -3,84 +3,85 @@
 Tests for bomber module
 """
 
+from py import test
 from bomber import GameState, Player
 import os
 
+class TestArena:
+    def test_tile_get(self):
+        state = GameState()
+        state.arena_load(["arenas", "test.bmm"])
+        assert state.tile_get(0, 0) == GameState.BLOCK
+        assert state.tile_get(1, 1) == GameState.SPAWN
+        assert state.tile_get(6, 6) == GameState.BLOCK
+
+        with test.raises(IndexError):
+            state.tile_get(-1, 0)
+
+        with test.raises(IndexError):
+            state.tile_get(0, -1)
+
+        with test.raises(IndexError):
+            state.tile_get(0, 7)
+
+        with test.raises(IndexError):
+            state.tile_get(7, 0)
+
+        with test.raises(IndexError):
+            state.tile_get(7, 7)
+
+    def test_coord_sanity(self):
+        state = GameState()
+        assert state.coord_sanity(0, 0) is None
+
+        with test.raises(IndexError):
+            state.coord_sanity(-1, 0)
+
+        with test.raises(IndexError):
+            state.coord_sanity(0, -1)
+
+        with test.raises(IndexError):
+            state.coord_sanity(-1, -1)
+
+    def test_tile_has(self):
+        state = GameState()
+        state.arena_load(["arenas", "test.bmm"])
+        assert state.tile_get(0, 0) == GameState.BLOCK
+        assert state.tile_has((0, 0), 0) # all tiles should have 0
+
 class TestGameState:
+    """
+    Tests for Game class
+    """
+
     def test_arena(self):
         """
         Default arena returned
         """
         state = GameState()
-        assert state.arena == """BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-BS ................................. SB
-B B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B B
-BS ................................. SB
-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"""
+        arena = open(os.sep.join(["arenas", "default.bmm"]), 'r')
+        assert str(state) == arena.read()
+        arena.close()
 
-    def test_load_arena(self):
+    def test_arena_load(self):
         """
         Load a arena from a file
         """
         state = GameState()
         arena = open(os.sep.join(["arenas", "test.bmm"]), 'r')
-        state.load_arena(os.sep.join(["arenas", "test.bmm"]))
+        state.arena_load(["arenas", "test.bmm"])
 
-        assert state.arena == arena.read()
+        assert str(state) == arena.read()
         arena.close()
 
-    def test_add_player(self):
+    def test_player_add(self):
         """
         Add a player to the game
         """
         state = GameState()
         player = Player()
-        state.add_player(player)
+        state.player_add(player)
         assert player in state.get_players()
-
-    def test_spawn_players(self):
-        """
-        Spawn (add to arena) players
-        """
-        state = GameState()
-        p1 = Player()
-        p2 = Player()
-        state.add_player(p1)
-        state.add_player(p2)
-        state.spawn()
-        assert state.arena == """BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-B1 ................................. 2B
-B B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B B
-BS ................................. SB
-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"""
 
     def test_player_number(self):
         """
@@ -88,14 +89,14 @@ BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"""
         """
         state = GameState()
         p1 = Player()
-        state.add_player(p1)
-        state.add_player(p1) # try and break with copies of p1
+        state.player_add(p1)
+        state.player_add(p1) # try and break with copies of p1
         state.spawn()
         assert p1.number == 1
 
-    def test_player_order(self):
+    def test_player_spawn(self):
         """
-        Check player numbers
+        Check player numbers and spawn positions
         """
         state = GameState()
         p1 = Player()
@@ -103,12 +104,12 @@ BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"""
         p3 = Player()
         p4 = Player()
         p5 = Player()
-        state.add_player(p1)
-        state.add_player(p1) # just to mess with stuff
-        state.add_player(p2)
-        state.add_player(p3)
-        state.add_player(p4)
-        state.add_player(p5) # too many players
+        state.player_add(p1)
+        state.player_add(p1) # just to mess with stuff
+        state.player_add(p2)
+        state.player_add(p3)
+        state.player_add(p4)
+        state.player_add(p5) # too many players
         state.spawn()
         assert p1.number == 1
         assert p2.number == 2
@@ -116,52 +117,119 @@ BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"""
         assert p4.number == 4
         assert p5.number == 0
 
-    def est_move(self): # TODO
+        assert p1.coords == (1, 1)
+        assert p2.coords == (37, 1)
+        assert p3.coords == (1, 17)
+        assert p4.coords == (37, 17)
+
+        assert state.tile_get(1, 1)   == GameState.P1
+        assert state.tile_get(37, 1)  == GameState.P2
+        assert state.tile_get(1, 17)  == GameState.P3
+        assert state.tile_get(37, 17) == GameState.P4
+
+    def test_player_current(self):
+        """
+        Player current action
+        """
+        p1 = Player()
+        p1.action_add(Player.DOWN)
+        p1.action_add(Player.UP)
+        assert p1.action_current() == Player.DOWN
+        assert p1.action_current() == Player.UP
+        assert p1.action_current() == Player.UP
+        p1.action_add(Player.LEFT)
+        assert p1.action_current() == Player.LEFT
+
+    def test_tile_add(self):
+        state = GameState()
+        coords = (1, 2)
+        assert state.tile_get(*coords) == GameState.SPACE
+        state.tile_add(coords, GameState.BLOCK)
+        assert state.tile_get(*coords) == GameState.BLOCK
+        assert state.tile_has(coords, GameState.SPACE)
+        assert state.tile_has(coords, GameState.BLOCK)
+
+    def test_tile_remove(self):
+        state = GameState()
+        coords = (1, 1)
+        assert state.tile_get(*coords) == GameState.SPAWN
+        state.tile_remove(coords, GameState.SPAWN)
+        assert state.tile_get(*coords) == GameState.SPACE
+
+    def test_move(self):
         """
         Move players
         """
         state = GameState()
         p1 = Player()
-        state.add_player(p1)
+        state.player_add(p1)
         state.spawn()
-        p1.act = Player.DOWN
+        assert state.tile_get(1, 1) == GameState.P1
+        assert state.tile_get(1, 2) == GameState.SPACE
+        state.player_move(p1, (1, 2))
+        assert state.tile_get(1, 1) == GameState.SPACE
+        assert state.tile_get(1, 2) == GameState.P1
+
+        assert state.tile_get(0, 0) == GameState.BLOCK
+        state.player_move(p1, (0, 0))
+        assert str(state) # asserting that state is still sane (i.e. player is not overlayed on block)
+        assert state.tile_get(1, 3) == GameState.DESTRUCTABLE
+        state.player_move(p1, (1, 3))
+        assert str(state) # asserting that state is still sane (i.e. player is not overlayed on destructable)
+
+    def test_tick(self):
+        """
+        Tick the game and check player position
+        """
+        state = GameState()
+        assert state.tick() == None
+        p1 = Player()
+        state.player_add(p1)
+        state.spawn()
+        assert state.tick() == None
+
+        coords_start = (1, 1)
+        coords_vt = (1, 2)
+        coords_hz = (2, 1)
+
+        assert state.tile_get(*coords_start) == GameState.P1
+        assert state.tile_get(*coords_vt) == GameState.SPACE
+        p1.action_add(Player.DOWN)
         state.tick()
-        # TODO: py.test exhibits weird behaviour if a test comparing to a triplequote fails -- investigate and patch?
-        assert state.arena == """BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
-B  ................................. 2B
-B1B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B
-B.....................................B
-B B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B.B B
-BS ................................. SB
-BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB"""
+        assert state.tile_get(*coords_start) == GameState.SPACE
+        assert state.tile_get(*coords_vt) == GameState.P1
+        p1.action_add(Player.UP)
+        state.tick()
+        assert state.tile_get(*coords_start) == GameState.P1
+        assert state.tile_get(*coords_vt) == GameState.SPACE
+        p1.action_add(Player.RIGHT)
+        state.tick()
+        assert state.tile_get(*coords_start) == GameState.SPACE
+        assert state.tile_get(*coords_hz) == GameState.P1
+        p1.action_add(Player.LEFT)
+        state.tick()
+        assert state.tile_get(*coords_start) == GameState.P1
+        assert state.tile_get(*coords_hz) == GameState.SPACE
 
 class TestPlayer:
+    """
+    Tests for Player class
+    """
+
     def test_actions(self):
         """
         A series of actions should be entered and pulled out properly
         """
         p1 = Player()
-        p1.act = Player.DOWN
-        p1.act = Player.LEFT
-        p1.act = Player.BOMB
-        p1.act = Player.RIGHT
-        p1.act = Player.RIGHT
-        p1.act = Player.RIGHT
-        assert p1.act == Player.DOWN
-        assert p1.act == Player.LEFT
-        assert p1.act == Player.BOMB
-        assert p1.act == Player.RIGHT
-        assert p1.act == Player.RIGHT
-        assert p1.act == Player.RIGHT
+        p1.action_add(Player.DOWN)
+        p1.action_add(Player.LEFT)
+        p1.action_add(Player.BOMB)
+        p1.action_add(Player.RIGHT)
+        p1.action_add(Player.RIGHT)
+        p1.action_add(Player.RIGHT)
+        assert p1.action_next() == Player.DOWN
+        assert p1.action_next() == Player.LEFT
+        assert p1.action_next() == Player.BOMB
+        assert p1.action_next() == Player.RIGHT
+        assert p1.action_next() == Player.RIGHT
+        assert p1.action_next() == Player.RIGHT
