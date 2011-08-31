d4f2deaf39979c0be71e5662f7cb426cd056b151
Adam Piper <adam@ahri.net>
2011-08-31 21:30:04 +0100

    Mods to client

--- client.py
+++ client.py
@@ -17,15 +17,25 @@ def state_command(base=BASE):
     rc = RestClientJson(base)
     print rc.get('state')
 
-def player_command(base=BASE, uid=None, action=None):
+def player_create_command(base=BASE):
     """Create a player"""
     rc = RestClientJson(base)
-    if uid is None:
-        print rc.post('player')
-    elif action is None:
-        print rc.get('player', uid)
-    else:
-        print rc.put('player', uid, dict(action=action))
+    print rc.post('player')
+
+def player_detail_command(uid, base=BASE):
+    rc = RestClientJson(base)
+    print rc.get('player', uid)
+
+def player_action_command(uid, action, base=BASE):
+    """Transmit an action for an existing player"""
+    rc = RestClientJson(base)
+    print rc.put('player', uid, dict(action=action))
+
+def player_name_command(uid, name, base=BASE):
+    """Name an existing player"""
+    rc = RestClientJson(base)
+    rc.set_debug(True)
+    print rc.put('player', uid, dict(name=name))
 
 if __name__ == '__main__':
     import scriptine
