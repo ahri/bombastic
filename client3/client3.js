8b9dba98a7a7b13a78dfa91192c1f6c30a232d49
Adam Piper <adam.piper@logica.com>
2011-09-05 10:36:34 +0100

    Rework client3 to use proper js OO and make it work

--- client3/client3.js
+++ client3/client3.js
@@ -1,101 +1,108 @@
-var lookup_prefix = 'img/',
-    lookup_refs = [],
-    lookup = {
-    ' ': 'tile.png',
-    'S': 'tile.png',
-    'B': 'block.png',
-    '.': 'destructible.png',
-    '+': 'flame_cross.png',
-    '-': 'flame_hz.png',
-    '>': 'flame_hz.png',
-    '<': 'flame_hz.png',
-    '|': 'flame_vt.png',
-    '^': 'flame_vt.png',
-    'v': 'flame_vt.png',
-    '1': 'p1.png',
-    '2': 'p2.png',
-    '3': 'p3.png',
-    '4': 'p4.png',
-    'b': 'powerup_bomb.png',
-    'f': 'powerup_flame.png',
-};
+function Game() {
+    var uid,
+        last_frame,
+        img_refs = [],
+        lookup_prefix = 'img/',
+        lookup = {
+            ' ': 'tile.png',
+            'S': 'tile.png',
+            'B': 'block.png',
+            'x': 'bomb.png',
+            '.': 'destructible.png',
+            '+': 'flame_cross.png',
+            '-': 'flame_hz.png',
+            '>': 'flame_hz.png',
+            '<': 'flame_hz.png',
+            '|': 'flame_vt.png',
+            '^': 'flame_vt.png',
+            'v': 'flame_vt.png',
+            '1': 'p1.png',
+            '2': 'p2.png',
+            '3': 'p3.png',
+            '4': 'p4.png',
+            'b': 'powerup_bomb.png',
+            'f': 'powerup_flame.png',
+        };
 
-function keys_register(uid) {
-    $(document).keydown(function(e) {
-        if (e.keyCode == 32) {
-            $.update('/player/' + uid, JSON.stringify({'action': 'BOMB'}));
-            return false;
-        } else if (e.keyCode == 37) {
-            $.update('/player/' + uid, JSON.stringify({'action': 'LEFT'}));
-            return false;
-        } else if (e.keyCode == 38) {
-            $.update('/player/' + uid, JSON.stringify({'action': 'UP'}));
-            return false;
-        } else if (e.keyCode == 39) {
-            $.update('/player/' + uid, JSON.stringify({'action': 'RIGHT'}));
-            return false;
-        } else if (e.keyCode == 40) {
-            $.update('/player/' + uid, JSON.stringify({'action': 'DOWN'}));
-            return false;
-        }
-    });
-}
+    var player_create = function () {
+        $.create('/player', JSON.stringify({'name': 'js'}), function(resp) {
+            uid = resp.uid;
+        });
+    }
 
-function player_create() {
-    $.create('/player', JSON.stringify({'name': 'js'}), function(resp) {
-        keys_register(resp.uid);
-        game_update(resp.uid);
-    });
-}
-
-function build_table() {
-    $.read('/game', function(resp) {
-        var table = document.getElementById('game'),
-            table_row = document.createElement('tr'),
-            table_cell,
-            img;
-
-        for (i = 0; i < resp.length; i++) {
-            var c = resp.charAt(i);
-            if (c == "\n") {
-                table.appendChild(table_row);
-                table_row = document.createElement('tr');
-                continue;
+    var keys_register = function () {
+        $(document).keydown(function(e) {
+            if (e.keyCode == 32) {
+                $.update('/player/' + uid, JSON.stringify({'action': 'BOMB'}));
+                return false;
+            } else if (e.keyCode == 37) {
+                $.update('/player/' + uid, JSON.stringify({'action': 'LEFT'}));
+                return false;
+            } else if (e.keyCode == 38) {
+                $.update('/player/' + uid, JSON.stringify({'action': 'UP'}));
+                return false;
+            } else if (e.keyCode == 39) {
+                $.update('/player/' + uid, JSON.stringify({'action': 'RIGHT'}));
+                return false;
+            } else if (e.keyCode == 40) {
+                $.update('/player/' + uid, JSON.stringify({'action': 'DOWN'}));
+                return false;
             }
-            table_cell = document.createElement('td');
-            img        = document.createElement('img');
-            lookup_refs[i] = img;
-            img.setAttribute('src', lookup_prefix + lookup[c]);
-            table_cell.appendChild(img);
-            table_row.appendChild(table_cell);
-        }
-    });
-}
+        });
+    }
 
-function game_update(uid) {
-    // refresh loop
-    /*setInterval(function() {
-        var last_game;
+    var table_build = function () {
+        $.read('/game', function(resp) {
+            var table = document.getElementById('game'),
+                table_row = document.createElement('tr'),
+                table_cell,
+                img;
+
+            last_frame = resp;
+
+            for (i = 0; i < resp.length; i++) {
+                var c = resp.charAt(i);
+                if (c == "\n") {
+                    table.appendChild(table_row);
+                    table_row = document.createElement('tr');
+                    continue;
+                }
+                table_cell = document.createElement('td');
+                img        = document.createElement('img');
+                img_refs[i] = img;
+                img.setAttribute('src', lookup_prefix + lookup[c]);
+                table_cell.appendChild(img);
+                table_row.appendChild(table_cell);
+            }
+        });
+    }
+
+    var table_update = function () {
         $.read('/player/' + uid, function(resp) {
             for (i = 0; i < resp.game.length; i++) {
                 var c  = resp.game.charAt(i);
-                var cl = last_game.charAt(i);
-
                 if (c == "\n") {
-                    alert('newline');
                     continue;
                 }
+                var cl = last_frame.charAt(i);
 
                 if (c != cl) {
-                    lookup_refs[i].setAttribute('src', lookup_prefix + lookup[c]);
+                    img_refs[i].setAttribute('src', lookup_prefix + lookup[c]);
                 }
             }
-            last_game = resp.game;
+            last_frame = resp.game;
         });
-    }, 100);*/
+    }
+
+    player_create();
+    keys_register();
+    table_build();
+
+    setInterval(function() {
+        table_update();
+    }, 100);
 }
 
 $(function() {
-    player_create();
-    build_table();
+    Game();
 });
